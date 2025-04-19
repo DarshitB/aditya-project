c1d9daf3b675476ee629a780571606ec1633ae85
darshit <darshitbhanderi2316@gmail.com>
2025-04-19 12:05:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { H1, H2, H3, B1, L } from "./ui/Typography";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <H1>(H1) Heading , 32px bold </H1>
+      <H2>This is a heading 2</H2>
+      <H3>This is a heading 3</H3>
+      <B1>
+        (B1) Body text , 16px Regular Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
+      </B1>
+      <L>This is a label</L>
     </div>
   );
 }
---
+++ src/ui/Typography.js
@@ -0,0 +1,19 @@
+export const H1 = ({ children }) => (
+  <h1 className="">{children}</h1>
+);
+
+export const H2 = ({ children }) => (
+  <h2 className="">{children}</h2>
+);
+
+export const H3 = ({ children }) => (
+  <h3 className="">{children}</h3>
+);
+
+export const B1 = ({ children }) => (
+  <p className="">{children}</p>
+);
+
+export const L = ({ children }) => (
+  <label className="">{children}</label>
+);
